6274da2b4915f89150d84aa3d0f7c249f7428f4b
antoinebayssac <antoine.bayssac@edu.devinci.fr>
2024-07-18 21:39:09 +0200

    update user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,7 +7,7 @@ use App\Repository\UserRepository;
 use ApiPlatform\Metadata\ApiResource;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\Common\Collections\ArrayCollection;
-use Symfony\Component\Serializer\Attribute\Groups;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use ApiPlatform\Metadata\GetCollection;
@@ -22,47 +22,46 @@ use App\State\UserPasswordHasherProcessor;
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
 #[ApiResource(
     operations: [
-        new Get(),
-        new GetCollection(),
+        new Get(processor: UserPasswordHasherProcessor::class, security: "is_granted('ROLE_PATRON')"),
+        new GetCollection(processor: UserPasswordHasherProcessor::class, security: "is_granted('ROLE_PATRON')"),
         new Post(processor: UserPasswordHasherProcessor::class, security: "is_granted('ROLE_PATRON')"),
         new Put(processor: UserPasswordHasherProcessor::class, security: "is_granted('ROLE_PATRON')"),
         new Patch(processor: UserPasswordHasherProcessor::class, security: "is_granted('ROLE_PATRON')"),
-        new Delete(processor: UserPasswordHasherProcessor::class, security: "is_granted('ROLE_PATRON')"),
+        new Delete(security: "is_granted('ROLE_PATRON')"),
     ],
+    normalizationContext: ['groups' => ['user:read']],
+    denormalizationContext: ['groups' => ['user:write']]
 )]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
-    #[ORM\Column]
+    #[ORM\Column(type: 'integer')]
+    #[Groups('user:read')]
     private ?int $id = null;
 
     #[ORM\Column(length: 180)]
+    #[Groups(['user:read', 'user:write'])]
     private ?string $email = null;
 
-    /**
-     * @var list<string> The user roles
-     */
     #[ORM\Column]
+    #[Groups(['user:read', 'user:write'])]
     private array $roles = [];
 
-    /**
-     * @var string The hashed password
-     */
     #[ORM\Column]
-    #[Groups('read')]
+    #[Groups('user:read')]
     private ?string $password = null;
 
-    #[Groups('write')]
+    #[Groups('user:write')]
     private ?string $plainPassword = null;
 
-    /**
-     * @var Collection<int, Commande>
-     */
+    #[ORM\Column(length: 255)]
+    #[Groups(['user:read', 'user:write'])]
+    private ?string $firstname = null;
+
     #[ORM\OneToMany(targetEntity: Commande::class, mappedBy: 'serveur')]
+    #[Groups('user:read')]
     private Collection $commandes;
-
-
 
     public function __construct()
     {
@@ -86,32 +85,19 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
-    /**
-     * A visual identifier that represents this user.
-     *
-     * @see UserInterface
-     */
     public function getUserIdentifier(): string
     {
         return (string) $this->email;
     }
 
-    /**
-     * @see UserInterface
-     * @return list<string>
-     */
     public function getRoles(): array
     {
         $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
         $roles[] = 'ROLE_USER';
 
         return array_unique($roles);
     }
 
-    /**
-     * @param list<string> $roles
-     */
     public function setRoles(array $roles): static
     {
         $this->roles = $roles;
@@ -119,10 +105,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
-    /**
-     * @see PasswordAuthenticatedUserInterface
-     */
-    public function getPassword(): string
+    public function getPassword(): ?string
     {
         return $this->password;
     }
@@ -134,30 +117,36 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
-    public function getPlainPassword(): string
+    public function getPlainPassword(): ?string
     {
         return $this->plainPassword;
     }
 
-    public function setPlainPassword(string $plainPassword): static
+    public function setPlainPassword(?string $plainPassword): static
     {
         $this->plainPassword = $plainPassword;
 
         return $this;
     }
 
-    /**
-     * @see UserInterface
-     */
-    public function eraseCredentials(): void
+    public function getFirstname(): ?string
     {
-        // If you store any temporary, sensitive data on the user, clear it here
-        // $this->plainPassword = null;
+        return $this->firstname;
     }
 
-    /**
-     * @return Collection<int, Commande>
-     */
+    public function setFirstname(string $firstname): static
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function eraseCredentials(): void
+    {
+        // Si vous stockez des données temporaires sensibles sur l'utilisateur, effacez-les ici
+        $this->plainPassword = null;
+    }
+
     public function getCommandes(): Collection
     {
         return $this->commandes;
@@ -176,7 +165,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function removeCommande(Commande $commande): static
     {
         if ($this->commandes->removeElement($commande)) {
-            // set the owning side to null (unless already changed)
             if ($commande->getServeur() === $this) {
                 $commande->setServeur(null);
             }
